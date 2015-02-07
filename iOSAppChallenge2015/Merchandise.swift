ce8d76c02e69887db7056683a847a9118c404b61
Isaiah Smith <jordankid93@gmail.com>
2015-02-06 23:43:19 -0500

    Add stub files for models

---
+++ iOSAppChallenge2015/Merchandise.swift
@@ -0,0 +1,13 @@
+//
+//  Merchandise.swift
+//  iOSAppChallenge2015
+//
+//  Created by Isaiah Smith on 2/6/15.
+//  Copyright (c) 2015 Isaiah Smith. All rights reserved.
+//
+
+import Foundation
+
+class Merchandise {
+    
+}
---
+++ iOSAppChallenge2015/Photo.swift
@@ -0,0 +1,13 @@
+//
+//  Photo.swift
+//  iOSAppChallenge2015
+//
+//  Created by Isaiah Smith on 2/6/15.
+//  Copyright (c) 2015 Isaiah Smith. All rights reserved.
+//
+
+import Foundation
+
+class Photo {
+    
+}
